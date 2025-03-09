1ab255588a89796e5b91622445cfcd4f31b96460
Mohd. Ayan <ayanshakeel02@gmail.com>
2025-03-09 12:11:06 +0530

    refactor

--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -10,7 +10,7 @@ const MyOrder = () => {
         if (!userEmail) return;
 
         try {
-            const response = await fetch("http://localhost:5000/api/myOrderData", {
+            const response = await fetch("https://food-hub-tdcu.onrender.com/api/myOrderData", {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
